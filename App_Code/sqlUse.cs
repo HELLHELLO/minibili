a27da3c73aa6e63e64d37a6b5dade78623686f50
HELLHELLO <498523614@qq.com>
2017-07-05 13:41:13 +0800

    创建了数据库操作类sqlUse,实现了其中的查询方法

---
+++ App_Code/sqlUse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// 从其他类接受对数据库的操作请求，返回查询结果或命令执行结果
+/// </summary>
+namespace MiniBli
+{
+    public class sqlUse
+    {
+        //protected string table { get; set; }
+       // protected string key { get; set; }
+       // protected string what { get; set; }
+        public sqlUse()
+        {
+        }
+        //向table表中，查询字段key的值为what的行，查询的结果存储于datatable对象中返回
+        public DataTable doSearch(string table, string key, string what)
+        {
+            string setting = "DefaultConnection";
+            var connString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings[setting];
+            SqlConnection conn = new SqlConnection(connString.ConnectionString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from {table} where {key}='{what}'";
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+    }
+}
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        MiniBli.sqlUse search = new MiniBli.sqlUse();
+        DataTable dt = search.doSearch("video", "Type", "test");
+        TextBox1.Text = dt.Rows[0]["location"].ToString();
     }
 }
